3b60efe2550acceb7a216bb4e7f1f4b883ab5fa2
Sarmad Wahab <me@bin.moe>
2023-01-09 13:18:09 -0600

    (refactor) Cleanup uploads query

--- Pfps.API/Models/UploadViewModel.cs
+++ Pfps.API/Models/UploadViewModel.cs
@@ -4,35 +4,9 @@ using Pfps.API.Data;
 
 namespace Pfps.API.Models
 {
-    public class UploadViewModel
+    public class UploadViewModel : UploadSimplifiedViewModel
     {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public bool IsApproved { get; set; }
-        public string[] Tags { get; set; }
-        public List<string> Urls { get; set; }
-        public PublicUserViewModel Uploader { get; set; }
-        public DateTime Timestamp { get; set; }
-        public int Views { get; set; }
-        public UploadType Type { get; set; }
-
-        public static UploadViewModel From(Upload upload)
-        {
-            return new UploadViewModel()
-            {
-                Id = upload.Id,
-                Title = upload.Title,
-                Description = upload.Description,
-                IsApproved = upload.IsApproved,
-                Tags = upload.Tags,
-                Uploader = PublicUserViewModel.From(upload.Uploader),
-                Urls = upload.Urls.ToList(),
-                Timestamp = upload.Timestamp,
-                Views = upload.Views,
-                Type = upload.Type,
-            };
-        }
+        // Not sure what the point of this was?
     }
 
     public class UploadSimplifiedViewModel
